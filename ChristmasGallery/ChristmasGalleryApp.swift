5524b0b1a219cb13b23b43e28bf0393e58d3e4e2
Adonai Domingez <donihernandez003@gmail.com>
2022-12-20 20:59:47 -0500

    Initial Commit

---
+++ ChristmasGallery/ChristmasGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasGalleryApp.swift
+//  ChristmasGallery
+//
+//  Created by Doni on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
